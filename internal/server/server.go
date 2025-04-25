1f988b83da0c0708cb8acfee0848146ab84934a3
10664kls <phuangpeht.phouminith@krungsri.com>
2025-04-25 10:17:54 +0700

    To define service endpoint

--- internal/server/server.go
+++ internal/server/server.go
@@ -43,10 +43,21 @@ func (s *Server) Install(e *echo.Echo, mws ...echo.MiddlewareFunc) error {
 	}
 
 	v1 := e.Group("/v1")
+	v1.POST("/auth/login", s.login)
+	v1.POST("/auth/token", s.refreshToken)
+	v1.GET("/auth/profile", s.authProfile, mws...)
 
-	v1.GET("/employees", s.listEmployees)
-	v1.GET("/employees/:id", s.getEmployeeByID)
-	v1.POST("/cards", s.createCard)
+	v1.GET("/employees", s.listEmployees, mws...)
+	v1.GET("/employees/:id", s.getEmployeeByID, mws...)
+	v1.GET("/employees/me/profile", s.getMyEmployeeProfile, mws...)
+
+	v1.POST("/business-cards", s.createBusinessCard, mws...)
+	v1.PUT("/business-cards/:id", s.updateBusinessCard, mws...)
+	v1.GET("/business-cards/me", s.listMyBusinessCards, mws...)
+	v1.GET("/business-cards/me/approval", s.listMyApprovalBusinessCards, mws...)
+	v1.GET("/business-cards/me/:id", s.getMyBusinessCardByID, mws...)
+	v1.GET("/business-cards", s.listBusinessCards, mws...)
+	v1.GET("/business-cards/:id", s.getBusinessCardByID, mws...)
 
 	return nil
 }
@@ -61,10 +72,20 @@ func badJSON() error {
 	return s.Err()
 }
 
+func badParam() error {
+	s, _ := rpcStatus.New(codes.InvalidArgument, "Request parameters must be a valid type.").
+		WithDetails(&edPb.ErrorInfo{
+			Reason: "BINDING_ERROR",
+			Domain: "http",
+		})
+
+	return s.Err()
+}
+
 func (s *Server) listEmployees(c echo.Context) error {
 	req := new(employee.EmployeeQuery)
 	if err := c.Bind(req); err != nil {
-		return badJSON()
+		return badParam()
 	}
 
 	ctx := c.Request().Context()
@@ -76,10 +97,14 @@ func (s *Server) listEmployees(c echo.Context) error {
 }
 
 func (s *Server) getEmployeeByID(c echo.Context) error {
-	id := c.Param("id")
+	req := new(employee.EmployeeQuery)
+	if err := c.Bind(req); err != nil {
+		return badParam()
+	}
+
 	ctx := c.Request().Context()
 
-	employee, err := s.employee.GetEmployeeByID(ctx, id)
+	employee, err := s.employee.GetEmployeeByID(ctx, req.ID)
 	if err != nil {
 		return err
 	}
@@ -88,19 +113,168 @@ func (s *Server) getEmployeeByID(c echo.Context) error {
 	})
 }
 
-func (s *Server) createCard(c echo.Context) error {
+func (s *Server) getMyEmployeeProfile(c echo.Context) error {
+	ctx := c.Request().Context()
+	employee, err := s.employee.GetMyEmployeeProfile(ctx)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, echo.Map{
+		"employeeProfile": employee,
+	})
+}
+
+func (s *Server) createBusinessCard(c echo.Context) error {
 	req := new(card.CardReq)
 	if err := c.Bind(req); err != nil {
 		return badJSON()
 	}
 
 	ctx := c.Request().Context()
-	card, err := s.card.CreateCard(ctx, req)
+	card, err := s.card.CreateBusinessCard(ctx, req)
 	if err != nil {
 		return err
 	}
 
 	return c.JSON(http.StatusOK, echo.Map{
 		"card": card,
+	})
+}
+
+func (s *Server) updateBusinessCard(c echo.Context) error {
+	req := new(card.CardReq)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	card, err := s.card.UpdateBusinessCard(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"card": card,
+	})
+}
+
+func (s *Server) listMyBusinessCards(c echo.Context) error {
+	req := new(card.CardQuery)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	cards, err := s.card.ListMyBusinessCards(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, cards)
+}
+
+func (s *Server) getMyBusinessCardByID(c echo.Context) error {
+	req := new(card.CardQuery)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	card, err := s.card.GetMyBusinessCardByID(ctx, req.ID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"card": card,
+	})
+}
+
+func (s *Server) listBusinessCards(c echo.Context) error {
+	req := new(card.CardQuery)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	cards, err := s.card.ListBusinessCards(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, cards)
+}
+
+func (s *Server) getBusinessCardByID(c echo.Context) error {
+	req := new(card.CardQuery)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	card, err := s.card.GetBusinessCardByID(ctx, req.ID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"card": card,
+	})
+}
+
+func (s *Server) listMyApprovalBusinessCards(c echo.Context) error {
+	req := new(card.CardQuery)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	cards, err := s.card.ListMyApprovalBusinessCards(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, cards)
+}
+
+func (s *Server) login(c echo.Context) error {
+	req := new(auth.LoginReq)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	token, err := s.auth.Login(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, token)
+}
+
+func (s *Server) refreshToken(c echo.Context) error {
+	req := new(auth.NewTokenReq)
+	if err := c.Bind(req); err != nil {
+		return badJSON()
+	}
+
+	ctx := c.Request().Context()
+	token, err := s.auth.RefreshToken(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, token)
+}
+
+func (s *Server) authProfile(c echo.Context) error {
+	ctx := c.Request().Context()
+	profile, err := s.auth.Profile(ctx)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"profile": profile,
 	})
 }
